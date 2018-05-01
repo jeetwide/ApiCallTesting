65d02a9f859ff7d02effe2759d3c07b6581c8bde
jeetwide <wide.angle456@gmail.com>
2018-05-01 12:30:21 -0700

    Added Youtube player

---
+++ app/src/main/java/com/example/myapitesting/ActYoutubePlayer.java
@@ -0,0 +1,53 @@
+package com.example.myapitesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ActYoutubePlayer extends YouTubeBaseActivity {
+
+
+    @BindView(R.id.btn)
+    Button btn;
+
+    @BindView(R.id.youtubtePlayerView)
+    YouTubePlayerView youtubtePlayerView;
+
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_youtube_player);
+        ButterKnife.bind(this);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                        youTubePlayer.loadVideo("3LiubyYpEUk");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    youtubtePlayerView.initialize(PlayerConfig.API_KEY,onInitializedListener);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/myapitesting/PlayerConfig.java
@@ -0,0 +1,9 @@
+package com.example.myapitesting;
+
+public class PlayerConfig {
+
+
+    PlayerConfig(){}
+
+    public static final String API_KEY="AIzaSyCn6Agou8xYMW89D3v-Nwz0br37VJmr0h0";
+}
